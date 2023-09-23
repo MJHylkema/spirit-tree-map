4493f801fc4a1d4bf098c67350a7b0148d633208
Mathew Hylkema <mathewhylkema@gmail.com>
2023-09-23 21:15:17 +1200

    Convert ExamplePlugin to SpiritTreeMapPlugin

--- src/main/java/com/mjhylkema/SpiritTreeMap/SpiritTreeMapPlugin.java
+++ src/main/java/com/mjhylkema/SpiritTreeMap/SpiritTreeMapPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.mjhylkema.SpiritTreeMap;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Spirit Tree Map"
 )
-public class ExamplePlugin extends Plugin
+public class SpiritTreeMapPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private SpiritTreeMapConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	SpiritTreeMapConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(SpiritTreeMapConfig.class);
 	}
 }
--- src/test/java/com/mjhylkema/SpiritTreeMap/SpiritTreeMapPluginTest.java
+++ src/test/java/com/mjhylkema/SpiritTreeMap/SpiritTreeMapPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.mjhylkema.SpiritTreeMap;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class SpiritTreeMapPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(SpiritTreeMapPlugin.class);
 		RuneLite.main(args);
 	}
 }
