b58e6ba1d4d7778c901a5d2a7905c14d8a1d6d57
Mathew Hylkema <mathewhylkema@gmail.com>
2024-01-20 19:19:17 +1300

    Show current spirit tree location on map - tracks GameObject spawns

---
+++ src/main/java/com/mjhylkema/TeleportMaps/definition/GameObjectDefinition.java
@@ -0,0 +1,11 @@
+package com.mjhylkema.TeleportMaps.definition;
+
+import lombok.Getter;
+
+@Getter
+public class GameObjectDefinition
+{
+	private int objectId;
+	private int worldPointX;
+	private int worldPointY;
+}
--- src/main/java/com/mjhylkema/TeleportMaps/definition/TreeDefinition.java
+++ src/main/java/com/mjhylkema/TeleportMaps/definition/TreeDefinition.java
@@ -5,8 +5,11 @@ import lombok.Getter;
 @Getter
 public class TreeDefinition extends AdventureLogEntryDefinition
 {
+	private int id;
 	private int width;
 	private int height;
 	private int spriteEnabled;
 	private int spriteHover;
+	private int spriteSelected;
+	private GameObjectDefinition treeObject;
 }
