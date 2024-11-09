50257a6658b9e380b0f179468e2a77f6d79a7dfc
Mathew Hylkema <mathewhylkema@gmail.com>
2024-11-09 17:10:49 +1300

    Match existing source formatting

--- src/main/java/com/mjhylkema/TeleportMaps/components/adventureLog/SkillsNecklaceMap.java
+++ src/main/java/com/mjhylkema/TeleportMaps/components/adventureLog/SkillsNecklaceMap.java
@@ -23,185 +23,204 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Slf4j
-public class SkillsNecklaceMap extends BaseMap implements IAdventureMap {
-  /* Definition JSON files */
-  private static final String DEF_FILE_SKILLS_NECKLACE = "/SkillsNecklaceMap/SkillsNecklaceDefinitions.json";
+public class SkillsNecklaceMap extends BaseMap implements IAdventureMap
+{
+	/* Definition JSON files */
+	private static final String DEF_FILE_SKILLS_NECKLACE = "/SkillsNecklaceMap/SkillsNecklaceDefinitions.json";
 
-  /* Sprite IDs, dimensions and positions */
-  private static final int MAP_SPRITE_ID = -19700;
-  private static final int MAP_SPRITE_WIDTH = 507;
-  private static final int MAP_SPRITE_HEIGHT = 319;
-  private static final int SCRIPT_TRIGGER_KEY = 1437;
-  private static final String LABEL_NAME_PATTERN = "<col=735a28>(.+)</col>: (<col=5f5f5f>)?(.+)";
-  private static final String TRAVEL_ACTION = "Travel";
-  private static final String EXAMINE_ACTION = "Examine";
-  private static final int ADVENTURE_LOG_CONTAINER_BACKGROUND = 0;
-  private static final int ADVENTURE_LOG_CONTAINER_TITLE = 1;
+	/* Sprite IDs, dimensions and positions */
+	private static final int MAP_SPRITE_ID = -19700;
+	private static final int MAP_SPRITE_WIDTH = 507;
+	private static final int MAP_SPRITE_HEIGHT = 319;
+	private static final int SCRIPT_TRIGGER_KEY = 1437;
+	private static final String LABEL_NAME_PATTERN = "<col=735a28>(.+)</col>: (<col=5f5f5f>)?(.+)";
+	private static final String TRAVEL_ACTION = "Travel";
+	private static final String EXAMINE_ACTION = "Examine";
+	private static final int ADVENTURE_LOG_CONTAINER_BACKGROUND = 0;
+	private static final int ADVENTURE_LOG_CONTAINER_TITLE = 1;
 
-  // There has got to be a better way to do this. All other jewelry seems to use the chat window.
-  // But they all say this same phrase. Lucky that skills necklace uses the Adventure Log.
-  private static final String MENU_TITLE = "Where would you like to teleport to?";
+	// There has got to be a better way to do this. All other jewelry seems to use the chat window.
+	// But they all say this same phrase. Lucky that skills necklace uses the Adventure Log.
+	private static final String MENU_TITLE = "Where would you like to teleport to?";
 
-  private SkillsNecklaceDefinition[] skillsNecklaceDefinitions;
-  private HashMap<String, SkillsNecklaceDefinition> skillsNecklaceDefinitionLookup;
-  private HashMap<String, AdventureLogEntry<SkillsNecklaceDefinition>> availableLocations;
+	private SkillsNecklaceDefinition[] skillsNecklaceDefinitions;
+	private HashMap<String, SkillsNecklaceDefinition> skillsNecklaceDefinitionLookup;
+	private HashMap<String, AdventureLogEntry<SkillsNecklaceDefinition>> availableLocations;
 
-  @Inject
-  public SkillsNecklaceMap(TeleportMapsPlugin plugin, TeleportMapsConfig config, Client client, ClientThread clientThread) {
-    super(plugin, config, client, clientThread, config.showSkillsNecklaceMap());
+	@Inject
+	public SkillsNecklaceMap(TeleportMapsPlugin plugin, TeleportMapsConfig config, Client client, ClientThread clientThread)
+	{
+		super(plugin, config, client, clientThread, config.showSkillsNecklaceMap());
 
-    this.loadDefinitions();
-    this.buildSkillsNecklaceDefinitionLookup();
-  }
+		this.loadDefinitions();
+		this.buildSkillsNecklaceDefinitionLookup();
+	}
 
-  @Override
-  public boolean matchesTitle(String title) {
-    return title.equals(MENU_TITLE); // Regex matches fails because title string contains ? character
-  }
+	@Override
+	public boolean matchesTitle(String title)
+	{
+		return title.equals(MENU_TITLE); // Regex matches fails because title string contains ? character
+	}
 
-  private void loadDefinitions() {
-    this.skillsNecklaceDefinitions = this.plugin.loadDefinitionResource(SkillsNecklaceDefinition[].class, DEF_FILE_SKILLS_NECKLACE);
-  }
+	private void loadDefinitions()
+	{
+		this.skillsNecklaceDefinitions = this.plugin.loadDefinitionResource(SkillsNecklaceDefinition[].class, DEF_FILE_SKILLS_NECKLACE);
+	}
 
-  private void buildSkillsNecklaceDefinitionLookup() {
-    this.skillsNecklaceDefinitionLookup = new HashMap<>();
-    for (SkillsNecklaceDefinition skillsNecklaceDefinition : this.skillsNecklaceDefinitions) {
-      // Place the skills necklace definition in the lookup table indexed by its name
-      this.skillsNecklaceDefinitionLookup.put(skillsNecklaceDefinition.getName(), skillsNecklaceDefinition);
-    }
-  }
+	private void buildSkillsNecklaceDefinitionLookup()
+	{
+		this.skillsNecklaceDefinitionLookup = new HashMap<>();
+		for (SkillsNecklaceDefinition skillsNecklaceDefinition : this.skillsNecklaceDefinitions)
+		{
+			// Place the skills necklace definition in the lookup table indexed by its name
+			this.skillsNecklaceDefinitionLookup.put(skillsNecklaceDefinition.getName(), skillsNecklaceDefinition);
+		}
+	}
 
-  @Subscribe
-  public void onConfigChanged(ConfigChanged e)
-  {
-    switch (e.getKey())
-    {
-      case TeleportMapsConfig.KEY_SHOW_SKILLS_NECKLACE_MAP:
-        this.setActive(config.showSkillsNecklaceMap());
-      default:
-        super.onConfigChanged(e);
-    }
-  }
+	@Subscribe
+	public void onConfigChanged(ConfigChanged e)
+	{
+		switch (e.getKey())
+		{
+			case TeleportMapsConfig.KEY_SHOW_SKILLS_NECKLACE_MAP:
+				this.setActive(config.showSkillsNecklaceMap());
+			default:
+				super.onConfigChanged(e);
+		}
+	}
 
-  @Override
-  public void buildInterface(Widget adventureLogContainer) {
-    this.hideAdventureLogContainerChildren(adventureLogContainer);
-    this.buildAvailableTeleportList();
+	@Override
+	public void buildInterface(Widget adventureLogContainer)
+	{
+		this.hideAdventureLogContainerChildren(adventureLogContainer);
+		this.buildAvailableTeleportList();
 
-    this.createMapWidget(adventureLogContainer);
-    this.createTeleportWidgets(adventureLogContainer);
-  }
+		this.createMapWidget(adventureLogContainer);
+		this.createTeleportWidgets(adventureLogContainer);
+	}
 
-  private void hideAdventureLogContainerChildren(Widget adventureLogContainer) {
-    Widget title = adventureLogContainer.getChild(ADVENTURE_LOG_CONTAINER_TITLE);
-    if (title != null)
-      title.setHidden(true);
-  }
+	private void hideAdventureLogContainerChildren(Widget adventureLogContainer)
+	{
+		Widget title = adventureLogContainer.getChild(ADVENTURE_LOG_CONTAINER_TITLE);
+		if (title != null)
+		{
+			title.setHidden(true);
+		}
+	}
 
-  /**
-   * NOTE: Not even sure if this is needed. Needs confirmation if Zeah needs to be unlocked first.
-   * Farming and Woodcutting might be blocked like Windertodt on a games necklace.
-   */
-  private void buildAvailableTeleportList() {
-    this.availableLocations = new HashMap<>();
+	/**
+	 * NOTE: Not even sure if this is needed. Needs confirmation if Zeah needs to be unlocked first.
+	 * Farming and Woodcutting might be blocked like Windertodt on a games necklace.
+	 */
+	private void buildAvailableTeleportList()
+	{
+		this.availableLocations = new HashMap<>();
 
-    // Compile the pattern that will match the teleport label
-    // and place the hotkey and teleport name into groups
-    Pattern labelPattern = Pattern.compile(LABEL_NAME_PATTERN);
+		// Compile the pattern that will match the teleport label
+		// and place the hotkey and teleport name into groups
+		Pattern labelPattern = Pattern.compile(LABEL_NAME_PATTERN);
 
-    // Get the parent widgets containing the teleport locations list
-    Widget teleportList = this.client.getWidget(InterfaceID.ADVENTURE_LOG, 3);
+		// Get the parent widgets containing the teleport locations list
+		Widget teleportList = this.client.getWidget(InterfaceID.ADVENTURE_LOG, 3);
 
-    // Fetch all teleport label widgets
-    Widget[] labelWidgets = teleportList.getDynamicChildren();
+		// Fetch all teleport label widgets
+		Widget[] labelWidgets = teleportList.getDynamicChildren();
 
-    for (Widget child : labelWidgets) {
-      String shortcutKey;
-      String disabledColor;
-      String teleportName;
+		for (Widget child : labelWidgets)
+		{
+			String shortcutKey;
+			String disabledColor;
+			String teleportName;
 
-      // Create a pattern matcher with the widgets text content
-      Matcher matcher = labelPattern.matcher(child.getText());
+			// Create a pattern matcher with the widgets text content
+			Matcher matcher = labelPattern.matcher(child.getText());
 
-      // If the text doesn't match the pattern, skip onto the next
-      if (!matcher.matches())
-        continue;
+			// If the text doesn't match the pattern, skip onto the next
+			if (!matcher.matches())
+			{
+				continue;
+			}
 
-      // Extract the pertinent information
-      shortcutKey = matcher.group(1);
-      disabledColor = matcher.group(2);
-      teleportName = matcher.group(3);
+			// Extract the pertinent information
+			shortcutKey = matcher.group(1);
+			disabledColor = matcher.group(2);
+			teleportName = matcher.group(3);
 
-      // Don't include unavailable teleports in available collection..
-      if (disabledColor != null)
-        continue;
+			// Don't include unavailable teleports in available collection..
+			if (disabledColor != null)
+			{
+				continue;
+			}
 
-      SkillsNecklaceDefinition skillsNecklaceDefinition = this.skillsNecklaceDefinitionLookup.get(teleportName);
+			SkillsNecklaceDefinition skillsNecklaceDefinition = this.skillsNecklaceDefinitionLookup.get(teleportName);
 
-      if (skillsNecklaceDefinition == null)
-        continue;
+			if (skillsNecklaceDefinition == null)
+			{
+				continue;
+			}
 
-      this.availableLocations.put(teleportName, new AdventureLogEntry<>(skillsNecklaceDefinition, child, shortcutKey));
-    }
-  }
+			this.availableLocations.put(teleportName, new AdventureLogEntry<>(skillsNecklaceDefinition, child, shortcutKey));
+		}
+	}
 
-  private void createMapWidget(Widget container)
-  {
-    this.createSpriteWidget(container,
-      MAP_SPRITE_WIDTH,
-      MAP_SPRITE_HEIGHT,
-      4,
-      12,
-      MAP_SPRITE_ID);
-  }
+	private void createMapWidget(Widget container)
+	{
+		this.createSpriteWidget(container,
+			MAP_SPRITE_WIDTH,
+			MAP_SPRITE_HEIGHT,
+			4,
+			12,
+			MAP_SPRITE_ID);
+	}
 
-  private void createTeleportWidgets(Widget container)
-  {
-    this.clearTeleports();
+	private void createTeleportWidgets(Widget container)
+	{
+		this.clearTeleports();
 
-    for (SkillsNecklaceDefinition skillsNecklaceDefinition : this.skillsNecklaceDefinitions)
-    {
-      Widget widgetContainer = container.createChild(-1, WidgetType.GRAPHIC);
-      Widget teleportWidget = container.createChild(-1, WidgetType.GRAPHIC);
+		for (SkillsNecklaceDefinition skillsNecklaceDefinition : this.skillsNecklaceDefinitions)
+		{
+			Widget widgetContainer = container.createChild(-1, WidgetType.GRAPHIC);
+			Widget teleportWidget = container.createChild(-1, WidgetType.GRAPHIC);
 
-      UITeleport teleport = new UITeleport(widgetContainer, teleportWidget);
-      teleport.setTeleportSprites(skillsNecklaceDefinition.getSpriteEnabled(), skillsNecklaceDefinition.getSpriteHover(), skillsNecklaceDefinition.getSpriteDisabled());
+			UITeleport teleport = new UITeleport(widgetContainer, teleportWidget);
+			teleport.setTeleportSprites(skillsNecklaceDefinition.getSpriteEnabled(), skillsNecklaceDefinition.getSpriteHover(), skillsNecklaceDefinition.getSpriteDisabled());
 
-      teleport.setPosition(skillsNecklaceDefinition.getX(), skillsNecklaceDefinition.getY());
-      teleport.setSize(skillsNecklaceDefinition.getWidth(), skillsNecklaceDefinition.getHeight());
-      teleport.setName(skillsNecklaceDefinition.getName());
+			teleport.setPosition(skillsNecklaceDefinition.getX(), skillsNecklaceDefinition.getY());
+			teleport.setSize(skillsNecklaceDefinition.getWidth(), skillsNecklaceDefinition.getHeight());
+			teleport.setName(skillsNecklaceDefinition.getName());
 
-      if (this.isLocationUnlocked(skillsNecklaceDefinition.getName()))
-      {
-        AdventureLogEntry<SkillsNecklaceDefinition> adventureLogEntry = this.availableLocations.get(skillsNecklaceDefinition.getName());
+			if (this.isLocationUnlocked(skillsNecklaceDefinition.getName()))
+			{
+				AdventureLogEntry<SkillsNecklaceDefinition> adventureLogEntry = this.availableLocations.get(skillsNecklaceDefinition.getName());
 
-        teleport.addAction(TRAVEL_ACTION, () -> this.triggerTeleport(adventureLogEntry));
+				teleport.addAction(TRAVEL_ACTION, () -> this.triggerTeleport(adventureLogEntry));
 
-        UIHotkey hotkey = this.createHotKey(container, skillsNecklaceDefinition.getHotkey(), adventureLogEntry.getKeyShortcut());
-        teleport.attachHotkey(hotkey);
-      }
-      else
-      {
-        teleport.setLocked(true);
-        teleport.addAction(EXAMINE_ACTION, () -> this.triggerLockedMessage(skillsNecklaceDefinition));
-      }
+				UIHotkey hotkey = this.createHotKey(container, skillsNecklaceDefinition.getHotkey(), adventureLogEntry.getKeyShortcut());
+				teleport.attachHotkey(hotkey);
+			}
+			else
+			{
+				teleport.setLocked(true);
+				teleport.addAction(EXAMINE_ACTION, () -> this.triggerLockedMessage(skillsNecklaceDefinition));
+			}
 
-      this.addTeleport(teleport);
-    }
+			this.addTeleport(teleport);
+		}
 
-  }
-  private boolean isLocationUnlocked(String teleportName)
-  {
-    return this.availableLocations.containsKey(teleportName);
-  }
+	}
 
-  private void triggerLockedMessage(SkillsNecklaceDefinition skillsNecklaceDefinition)
-  {
-    this.clientThread.invokeLater(() -> this.client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", String.format("You are unable to travel to %s.", skillsNecklaceDefinition.getName()), null));
-  }
+	private boolean isLocationUnlocked(String teleportName)
+	{
+		return this.availableLocations.containsKey(teleportName);
+	}
 
-  private void triggerTeleport(AdventureLogEntry<SkillsNecklaceDefinition> adventureLogEntry)
-  {
-    this.clientThread.invokeLater(() -> this.client.runScript(SCRIPT_TRIGGER_KEY, this.client.getWidget(0xBB0003).getId(), adventureLogEntry.getWidget().getIndex()));
-  }
+	private void triggerLockedMessage(SkillsNecklaceDefinition skillsNecklaceDefinition)
+	{
+		this.clientThread.invokeLater(() -> this.client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", String.format("You are unable to travel to %s.", skillsNecklaceDefinition.getName()), null));
+	}
+
+	private void triggerTeleport(AdventureLogEntry<SkillsNecklaceDefinition> adventureLogEntry)
+	{
+		this.clientThread.invokeLater(() -> this.client.runScript(SCRIPT_TRIGGER_KEY, this.client.getWidget(0xBB0003).getId(), adventureLogEntry.getWidget().getIndex()));
+	}
 }
--- src/main/java/com/mjhylkema/TeleportMaps/definition/SkillsNecklaceDefinition.java
+++ src/main/java/com/mjhylkema/TeleportMaps/definition/SkillsNecklaceDefinition.java
@@ -3,11 +3,12 @@ package com.mjhylkema.TeleportMaps.definition;
 import lombok.Getter;
 
 @Getter
-public class SkillsNecklaceDefinition extends AdventureLogEntryDefinition {
-  private int id;
-  private int width;
-  private int height;
-  private int spriteEnabled;
-  private int spriteHover;
-  private int spriteDisabled;
+public class SkillsNecklaceDefinition extends AdventureLogEntryDefinition
+{
+	private int id;
+	private int width;
+	private int height;
+	private int spriteEnabled;
+	private int spriteHover;
+	private int spriteDisabled;
 }
